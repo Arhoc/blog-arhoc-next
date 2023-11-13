06c48a618f3c04883a2cb48d66354c5cd1c909e0
Arhoc <la_archi@protonmail.com>
2023-11-12 19:49:46 -0600

    Temp SQL fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
-import { sql } from "@vercel/postgres";
+//import { sql } from "@vercel/postgres";
 
 const inter = Inter({ subsets: ['latin'] })
 
