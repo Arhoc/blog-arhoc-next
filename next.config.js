c914c7dd4550274f5005e3511b2a9452e44c272d
Arhoc <la_archi@protonmail.com>
2023-11-19 20:18:39 -0600

    Axiom-Next

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const { withAxiom } = require('next-axiom');
+
+module.exports = withAxiom({
+  // ... your existing config
+  reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+})
+
+/*const nextConfig = {
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
@@ -8,4 +21,4 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig*/
